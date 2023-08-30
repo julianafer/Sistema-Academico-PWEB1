e9a29b55b672db4f55a32a3fd7dd9b7590840172
Juliana Ferreira <julianafercavalcante@hotmail.com>
2023-08-29 23:10:44 -0300

    criado controlador

--- controlador/disciplina_controlador.js
+++ controlador/disciplina_controlador.js
@@ -4,4 +4,20 @@ class DisciplinaControlador {
         this.servico = new DisciplinaService();
     }
 
+    inserir() {
+        const nomeDisciplina = document.querySelector("#nomeDisciplina");
+        const codigoDisciplina = document.querySelector("codigoDisciplina");
+        const disciplinaInserida = this.servico.inserir(codigoDisciplina.ariaValueMax, nomeDisciplina.value);
+        const listaDisciplinasElemento = document.querySelector("#listaDisciplinas");
+        if (disciplinaInserida) {
+            this.inserirDisciplinaNoHtml(disciplinaInserida, listaDisciplinasElemento);
+        }
+    }
+
+    inserirDisciplinaNoHtml(disciplina, elementoDestino) {
+        const disciplinaElemento = document.createElement("li");
+        disciplinaElemento.textContent = `Disciplina: ${disciplina.nome}`;
+        elementoDestino.appendChild(disciplinaElemento);
+    }
+
 }
